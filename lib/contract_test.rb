e33aa69894b8a0f069ccae8a72c58242fa38cf23
nagraham <nagraham00@hotmail.com>
2015-11-29 13:38:54 -0800

    Add benchmarking comparisons

--- lib/contract_test.rb
+++ lib/contract_test.rb
@@ -1,5 +1,5 @@
 require 'contracts'
-
+require 'benchmark'
 
 class User
   
@@ -17,12 +17,17 @@ class User
 end
 
 module ContractTest
+  
   include Contracts::Core
   C = Contracts
   
-  # Basic sanity test to make sure this works
+  # Sanity test to make sure the Contract gem is set up correctly
   Contract C::Num, C::Num => C::Num
   def self.add(a, b)
+    a + b
+  end
+  
+  def self.add_no_contract(a, b)
     a + b
   end
    
@@ -31,8 +36,29 @@ module ContractTest
   def self.create_user(request = {})
     return User.new(request)
   end
+  
+  def self.create_user_no_contract(request = {})
+    return User.new(request)
+  end
+  
 end
 
-puts ContractTest.add(2, 3) 
+# Helper method to compare the performance of methods with Contracts
+def compare_performance(entity, method, *args)
+  no_contract_label = "#{entity}:#{method}_no_contract"
+  with_contract_label = "#{entity}:#{method}"
+  
+  Benchmark.bm(no_contract_label.length) do |x|
+    x.report(no_contract_label) { entity.send("#{method}_no_contract", *args) }
+    x.report(with_contract_label) { entity.send(method, *args) }
+  end
+end
+
+#
+# --- Executable ---
+#
+p ContractTest.add(2, 3)
+compare_performance(ContractTest, "add", 2, 3)
+
 p ContractTest.create_user(name: "Alex")
-p ContractTest.create_user()
+compare_performance(ContractTest, "create_user", {name: "Alex"})
